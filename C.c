8401fd02ae809d856a0a2bbb0d1a205db6bdc5c4
ViktorKucherov <ViktorKucherov@outlook.com>
2019-01-20 16:09:37 +0400

    BRO!

--- C.c
+++ C.c
@@ -3,6 +3,10 @@
 int main () {
 
 printf("What did u sa 'bout my mom?");
+printf("What did u sa 'bout my mom?");
+printf("What did u sa 'bout my mom?");
+printf("What did u sa 'bout my mom?");
+printf("What did u sa 'bout my mom?");
 
 return 0;
 
